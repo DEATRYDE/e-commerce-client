f8d424c03eee4d644ea80b9d7110b1772cdb9837
Rahul Dogra <rahuldogra1998@gmail.com>
2021-06-07 19:22:16 +0530

    Register User Reducer

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,18 +1,38 @@
-import { SET_CURRENT_USER } from "../actions/types";
+import {
+  SET_CURRENT_USER,
+  SUCCESSFUL_REGISTER,
+  ERRORS,
+} from "../actions/types";
 import { isEmpty } from "lodash";
 
 const initialState = {
   isAutheticated: false,
   user: {},
+  token: localStorage.getItem("token"),
 };
 
 export default function authReducer(state = initialState, action) {
-  switch (action.type) {
+  const { type, payload } = action;
+  switch (type) {
     case SET_CURRENT_USER:
       return {
         ...state,
-        isAutheticated: !isEmpty(action.payload),
-        user: action.payload,
+        isAutheticated: !isEmpty(payload),
+        user: payload,
+      };
+    case SUCCESSFUL_REGISTER:
+      localStorage.setItem("token", payload.token);
+      return {
+        ...state,
+        ...payload,
+        isAutheticated: true,
+      };
+    case ERRORS:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        token: null,
+        isAutheticated: false,
       };
     default:
       return state;
