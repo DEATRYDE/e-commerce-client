9578906e1496e37e48d2c64d79d16c58013c6c59
Rahul Dogra <rahuldogra1998@gmail.com>
2021-08-10 13:26:23 +0530

    docker files

--- src/components/customers/Payment.js
+++ src/components/customers/Payment.js
@@ -2,22 +2,31 @@ import React, { Component } from "react";
 import StripeCheckout from "react-stripe-checkout";
 import axios from "axios";
 import { getServer } from "../../util";
+import { message } from "antd";
 
-class Payment extends Component {
+export default class Payment extends Component {
   constructor(props) {
     super(props);
     this.handleToken = this.handleToken.bind(this);
   }
+
   async handleToken(token) {
     const config = {
-      header: {
+      headers: {
         "Content-Type": "application/json",
       },
     };
     const context = { token, cart: this.props.cart, total: this.props.total };
-    await axios.post(`${getServer()}/api/payment`, context, config);
+    const res = await axios.post(`${getServer()}/api/payment`, context, config);
+    console.log(res.data);
+    if (res.data.status === 200) {
+      message.success("Thank you for the payment");
+    } else {
+      message.error("something went wrong");
+    }
   }
   render() {
+    console.log(this.props);
     return (
       <div>
         <StripeCheckout
@@ -32,4 +41,3 @@ class Payment extends Component {
     );
   }
 }
-export default Payment;
